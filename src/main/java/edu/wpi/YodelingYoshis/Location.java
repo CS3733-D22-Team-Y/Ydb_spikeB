67722606c0a3c06aa9bc327b32115c81984f92db
Nathan <ndpollock@wpi.edu>
2022-03-22 23:35:20 -0400

    Location Class constructor

--- src/main/java/edu/wpi/YodelingYoshis/Location.java
+++ src/main/java/edu/wpi/YodelingYoshis/Location.java
@@ -8,4 +8,15 @@ public class Location {
     String nodeType;
     String longName;
     String shortName;
+
+    public Location(String nodeID, int xCoord, int yCoord, int floor, String building, String nodeType, String longName, String shortName){
+        this.nodeID = nodeID;
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
+        this.floor = floor;
+        this.building = building;
+        this.nodeType = nodeType;
+        this.longName = longName;
+        this.shortName = shortName;
+    }
 }
