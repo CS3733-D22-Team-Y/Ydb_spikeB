5682b4ed6dea80f061cca3f57bd8a9fb9cc879a9
Nathan <ndpollock@wpi.edu>
2022-03-23 23:55:55 -0400

    adding location data manager

---
+++ src/main/java/edu/wpi/YodelingYoshis/LocationDataManager.java
@@ -0,0 +1,12 @@
+package edu.wpi.YodelingYoshis;
+
+import java.util.ArrayList;
+
+public class LocationDataManager {
+    private ArrayList<Location> locations = new ArrayList<>();
+
+    public void addLocation(Location location) {
+        locations.add(location);
+    }
+
+}
