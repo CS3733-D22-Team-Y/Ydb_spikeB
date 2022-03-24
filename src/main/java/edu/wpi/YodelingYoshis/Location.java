746826ca1757f644155b634cc57bdb8357e85f9a
echo532 <84986609+echo532@users.noreply.github.com>
2022-03-23 23:04:39 -0400

    edited location class, added ReadIn class
    1) Location class was considering Floor an int, however there are floors such as L1, so this was changed to a String val
    2) added in functionality to read in a csv file that is stored in main, into ArrayList. This function is self-contained and can be moved into another class

--- src/main/java/edu/wpi/YodelingYoshis/Location.java
+++ src/main/java/edu/wpi/YodelingYoshis/Location.java
@@ -1,22 +1,30 @@
 package edu.wpi.YodelingYoshis;
 
 public class Location {
-    String nodeID;
-    int xCoord,yCoord;
-    int floor;
-    String building;
-    String nodeType;
-    String longName;
-    String shortName;
+  String nodeID;
+  int xCoord, yCoord;
+  String floor;
+  String building;
+  String nodeType;
+  String longName;
+  String shortName;
 
-    public Location(String nodeID, int xCoord, int yCoord, int floor, String building, String nodeType, String longName, String shortName){
-        this.nodeID = nodeID;
-        this.xCoord = xCoord;
-        this.yCoord = yCoord;
-        this.floor = floor;
-        this.building = building;
-        this.nodeType = nodeType;
-        this.longName = longName;
-        this.shortName = shortName;
-    }
+  public Location(
+      String nodeID,
+      int xCoord,
+      int yCoord,
+      String floor,
+      String building,
+      String nodeType,
+      String longName,
+      String shortName) {
+    this.nodeID = nodeID;
+    this.xCoord = xCoord;
+    this.yCoord = yCoord;
+    this.floor = floor;
+    this.building = building;
+    this.nodeType = nodeType;
+    this.longName = longName;
+    this.shortName = shortName;
+  }
 }
--- src/main/java/edu/wpi/YodelingYoshis/Main.java
+++ src/main/java/edu/wpi/YodelingYoshis/Main.java
@@ -1,9 +1,16 @@
 package edu.wpi.YodelingYoshis;
 
+import java.io.FileNotFoundException;
+
 public class Main {
 
-  public static void main(String[] args) {
-    System.out.println("yest");
-    App.launch(App.class, args);
+  public static void main(String[] args) throws FileNotFoundException {
+    // System.out.println("yest");
+
+    ReadIn thisRead = new ReadIn();
+    thisRead.readCSV();
+
+    System.out.println("completed");
+    // App.launch(App.class, args);
   }
 }
