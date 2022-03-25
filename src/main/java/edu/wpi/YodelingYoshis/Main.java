400eed9f53f13d9502a8441119b11a29b5ca557f
DataPointBeing <jcunown@live.com>
2022-03-25 00:13:15 -0400

    foo

--- src/main/java/edu/wpi/YodelingYoshis/Main.java
+++ src/main/java/edu/wpi/YodelingYoshis/Main.java
@@ -12,16 +12,6 @@ public class Main {
     LocationDataManager dm = new LocationDataManager(db_conn);
     dm.cleanTable("locations");
 
-    Location location = new Location("boo", 5, 5, "1", "building", "node", "lN", "sN");
-    Location loc1 = new Location("raw", 5, 5, "1", "building", "node", "lN", "sN");
-    Location lo2 = new Location("foo", 5, 5, "1", "building", "node", "lN", "sN");
-    Location lo3 = new Location("bar", 5, 5, "1", "building", "node", "lN", "sN");
-
-    dm.addLocation(location);
-    dm.addLocation(loc1);
-    dm.addLocation(lo2);
-    dm.addLocation(lo3);
-
     LocationManagerInterface locationManagerInterface = new LocationManagerInterface();
     locationManagerInterface.start();
     //    App.launch(App.class, args);
