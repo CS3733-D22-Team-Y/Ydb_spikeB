cbd80a0f7fcb6f0905db1f3647f0e250492c6d54
DataPointBeing <jcunown@live.com>
2022-03-25 00:36:57 -0400

    documentation

--- src/main/java/edu/wpi/YodelingYoshis/LocationManagerFunctionality.java
+++ src/main/java/edu/wpi/YodelingYoshis/LocationManagerFunctionality.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 
 public class LocationManagerFunctionality {
 
+  /**
+   * In-between for displaying the list of database nodes.
+   */
   public static void displayNodes() {
     ArrayList<Location> locs = LocationDataManager.getLocations();
 
@@ -27,6 +30,13 @@ public class LocationManagerFunctionality {
     }
   }
 
+  /**
+   * In-between: replaces the floor and location type of the node w/ specified ID.
+   *
+   * @param ID Node ID
+   * @param floor new floor value
+   * @param locationType new location type
+   */
   public static boolean replaceNodeVals(String ID, String floor, String locationType) {
     Location toChange = LocationDataManager.getLocationCopy(ID);
     if (toChange == null) {
@@ -39,15 +49,30 @@ public class LocationManagerFunctionality {
     return LocationDataManager.replaceLocation(ID, toChange);
   }
 
+  /**
+   * In-between: creates and enters an empty node with the given ID.
+   *
+   * @param ID Node ID
+   */
   public static boolean newNode(String ID) {
     Location newLoc = new Location(ID);
     return LocationDataManager.addLocation(newLoc);
   }
 
+  /**
+   * In-between: deletes the node with the given ID
+   *
+   * @param ID Node ID
+   */
   public static boolean deleteNode(String ID) {
     return LocationDataManager.removeLocation(ID);
   }
 
+  /**
+   * In-between for dumping database contents to CSV with given name.
+   *
+   * @param fileLoc file location to write to
+   */
   public static boolean writeDbToCSV(String fileLoc) {
     try {
       Locations2CSV.generateCSV(fileLoc);
