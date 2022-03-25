acd09e6f5f3e073b5352f83f2efb917cc9acd82e
echo532 <84986609+echo532@users.noreply.github.com>
2022-03-25 00:27:00 -0400

    fixed CSV

--- src/main/java/edu/wpi/YodelingYoshis/Main.java
+++ src/main/java/edu/wpi/YodelingYoshis/Main.java
@@ -7,28 +7,28 @@ public class Main {
 
   public static void main(String[] args) {
 
-    //creates database
+    // creates database
     Database locationDB = new Database("LocationDB");
-    Connection db_conn = locationDB.connection; //establishes connection to database
+    Connection db_conn = locationDB.connection; // establishes connection to database
 
-    //creating dataManager class that manages database
+    // creating dataManager class that manages database
     LocationDataManager dataManager = new LocationDataManager(db_conn);
     dataManager.cleanTable("locations");
 
-    //function set to read in CSV
+    // function set to read in CSV
     ReadIn input = new ReadIn();
     ArrayList<Location> locationArray = new ArrayList<Location>();
     locationArray = input.readCSV();
 
-    //add ArrayList locations so that Datamanager can access all values
+    // add ArrayList locations so that Datamanager can access all values
     dataManager.addLocations(locationArray);
 
-    //creatae locationManager to run console loop
+    // creatae locationManager to run console loop
     LocationManagerInterface locationManagerInterface = new LocationManagerInterface();
     locationManagerInterface.start();
     //    App.launch(App.class, args);
 
-    //once ended, shuts off the database
+    // once ended, shuts off the database
     locationDB.shutdown_db();
   }
 }
